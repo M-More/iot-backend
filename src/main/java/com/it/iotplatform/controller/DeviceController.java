729953d4c9219cf701dbb61bb679335832235aed
M-More <710599679@qq.com>
2021-11-19 10:49:31 +0800

    first commit

---
+++ src/main/java/com/it/iotplatform/controller/DeviceController.java
@@ -0,0 +1,16 @@
+package com.it.iotplatform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/device")
+public class DeviceController {
+
+    @GetMapping(value ="/getDevice")
+    public String getDevice(){
+        return "device1";
+    }
+}
