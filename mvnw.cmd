e2388ab9850bfe8ee4fa38a7da36cbbd4f9a0716
M-More <710599679@qq.com>
2021-11-19 14:12:48 +0800

    device 10

--- mvnw.cmd
+++ mvnw.cmd
@@ -74,7 +74,7 @@ echo.
 echo Error: JAVA_HOME is set to an invalid directory. >&2
 echo JAVA_HOME = "%JAVA_HOME%" >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
-echo location of your Java installation. >&2
+echo location of your Java installation. >&2gi
 echo.
 goto error
 
