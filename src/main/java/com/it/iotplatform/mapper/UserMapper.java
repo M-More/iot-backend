a04bcccc46baf8b8c5e657afe423a90bfc430721
caoqingyu <943203353@qq.com>
2021-11-24 23:38:29 +0800

    After check userpassword return message and information about user complete &appresponse new constructor and bulid method

--- src/main/java/com/it/iotplatform/mapper/UserMapper.java
+++ src/main/java/com/it/iotplatform/mapper/UserMapper.java
@@ -4,9 +4,12 @@ import com.it.iotplatform.model.AppResponse;
 import com.it.iotplatform.model.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
     AppResponse<User> addUser(User user);
     String getUserPswByAccount(String userAccount);
     String getPhoneByName(String userName);
+    List<User> getUserByAccount(String userAccount);
 }
